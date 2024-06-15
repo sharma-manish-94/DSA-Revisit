55e87f9e9bcfb1ea56882104f175c1ac2e76c2cb
code-manish <code.manish94@gmail.com>
2024-06-15 22:20:00 +0530

    deletion in doubly linked list

--- linkedlist/DeleteDLL.java
+++ linkedlist/DeleteDLL.java
@@ -22,7 +22,7 @@ public class DeleteDLL {
 		Node forth = new Node(4);
 		Node fifth = new Node(5);
 		Node sixth = new Node(6);
-		
+
 		head.next = second;
 
 		second.prev = head;
@@ -46,25 +46,44 @@ public class DeleteDLL {
 		if (head == null || node == null) {
 			return head;
 		}
-		
+		// delete first element
+		if (node == head) {
+			head = head.next;
+			return head;
+		}
+		// delete last element
+		if (node.next == null) {
+			node.prev.next = null;
+		}
+
+		// delete middle element;
+		if (node.prev != null) {
+			node.prev.next = node.next;
+		}
+		if (node.next != null) {
+			node.next.prev = node.prev;
+		}
+		return head;
 
 	}
-	
+
 	private static void printLinkedList(Node head) {
-		if(head == null) {
+		if (head == null) {
 			System.out.println("Empty Linked List");
 		} else {
-			while(head != null) {
-				System.out.print(head.data+" ");
+			while (head != null) {
+				System.out.print(head.data + " ");
 				head = head.next;
 			}
 		}
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Original Linked List");
 		Node head = createLL();
 		printLinkedList(head);
-//		Node head = deleteLL();
-//		printLinkedList(head);
+		System.out.println("\nLinked List after deletion");
+		head = deleteLL(head, head.next);
+		printLinkedList(head);
 	}
 }
